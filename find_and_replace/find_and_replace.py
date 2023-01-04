5ffb34b66016c8d678fd7b2253f91a4fdddfa382
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-01-04 16:51:15 +0000

    replaceWord

--- find_and_replace/find_and_replace.py
+++ find_and_replace/find_and_replace.py
@@ -1,1 +1,5 @@
-hello
+def replace_word():
+    str = input("Enter your string")
+    find = input("Enter the word to replace")
+    replace = input("Enter the word replacement")
+    print(str.replace(find, replace))
