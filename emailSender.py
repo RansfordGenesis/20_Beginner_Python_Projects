0ab9d5c094bf15fdb5307980d8fa447ac72a6290
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-01-04 13:25:25 +0000

    emailSender

---
+++ emailSender.py
@@ -0,0 +1,24 @@
+from email.message import EmailMessage
+import ssl, smtplib
+
+email_sender = 'jefferykyeigenesis@gmail.com'
+email_password = 'husuzspwvtwxjdqe'
+
+email_receiver = 'ransfordgenesis@gmail.com'
+
+subject = 'Health'
+body = """
+How're you?
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
