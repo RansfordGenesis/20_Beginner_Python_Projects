256602cdb2ff18e2caae66fd0dbdd42f47d51591
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-01-05 13:51:35 +0000

    simple_calculator

--- calculator.py
+++ calculator.py
@@ -24,3 +24,25 @@ if choice.upper() == 'A':
     a = int(input("Input first integer: "))
     b = int(input("Input second integer: "))
     add(a, b)
+
+elif choice.upper() == 'B':
+    print("Subtraction")
+    a = int(input("Input first integer: "))
+    b = int(input("Input second integer: "))
+    sub(a, b)
+
+elif choice.upper() == 'C':
+    print("Multiplication")
+    a = int(input("Input first integer: "))
+    b = int(input("Input second integer: "))
+    mul(a, b)
+    
+elif choice.upper() == 'D':
+    print("Division")
+    a = int(input("Input first integer: "))
+    b = int(input("Input second integer: "))
+    div(a, b)
+    
+elif choice.upper() == 'E':
+    print("Thank You for trying my Calculator")
+    quit()
