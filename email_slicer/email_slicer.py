c99614dca0db2f10918f1600f80e98c78aa3a782
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-01-05 15:33:22 +0000

    email_slicer

--- email_slicer/email_slicer.py
+++ email_slicer/email_slicer.py
@@ -1,1 +1,14 @@
-Hello
+def email_slicer():
+    print("Welcome to Email Slicer")
+    print("")
+    
+    email = input("Input a valid email to slice: ")
+    (username, domain) = email.split("@")
+    (domain, extension) = domain.split(".")
+    
+    print("Username: ", username)
+    print("Domain: ", domain)
+    print("Extension: ", extension)
+
+
+email_slicer()
