c67ef6c8fec8dbaca9384f5a1c8476c32cef8000
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-01-05 14:01:10 +0000

    calculator

--- calculator.py
+++ calculator.py
@@ -14,35 +14,36 @@ def div(a, b):
     answer = a / b
     print(answer)
 
-print("Welcome to my simple Calculator\n")
-print("\n A. Addition\n B. Subtraction\n C. Multiplication\n D. Division\n E. Quit\n")
+while True:
+    print("Welcome to my simple Calculator")
+    print(" A. Addition\n B. Subtraction\n C. Multiplication\n D. Division\n E. Quit")
 
-choice = input("Input your choice: ")
+    choice = input("Input your choice: ")
 
-if choice.upper() == 'A':
-    print("Addition")
-    a = int(input("Input first integer: "))
-    b = int(input("Input second integer: "))
-    add(a, b)
+    if choice.upper() == 'A':
+        print("Addition")
+        a = int(input("Input first integer: "))
+        b = int(input("Input second integer: "))
+        add(a, b)
 
-elif choice.upper() == 'B':
-    print("Subtraction")
-    a = int(input("Input first integer: "))
-    b = int(input("Input second integer: "))
-    sub(a, b)
+    elif choice.upper() == 'B':
+        print("Subtraction")
+        a = int(input("Input first integer: "))
+        b = int(input("Input second integer: "))
+        sub(a, b)
 
-elif choice.upper() == 'C':
-    print("Multiplication")
-    a = int(input("Input first integer: "))
-    b = int(input("Input second integer: "))
-    mul(a, b)
-    
-elif choice.upper() == 'D':
-    print("Division")
-    a = int(input("Input first integer: "))
-    b = int(input("Input second integer: "))
-    div(a, b)
-    
-elif choice.upper() == 'E':
-    print("Thank You for trying my Calculator")
-    quit()
+    elif choice.upper() == 'C':
+        print("Multiplication")
+        a = int(input("Input first integer: "))
+        b = int(input("Input second integer: "))
+        mul(a, b)
+        
+    elif choice.upper() == 'D':
+        print("Division")
+        a = int(input("Input first integer: "))
+        b = int(input("Input second integer: "))
+        div(a, b)
+        
+    elif choice.upper() == 'E':
+        print("Thank You for trying my Calculator.")
+        quit()
